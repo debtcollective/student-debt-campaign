24b31c50740778ed31e4abe091a9b944080f974a
Alexis Duran <alexis.duranm@gmail.com>
2019-08-16 18:43:20 +0200

    refactor(components): static update on components and remove "Features" component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
 
 import logo from "../img/logo.svg";
 
@@ -13,33 +12,6 @@ const Footer = class extends React.Component {
             alt="Kaldi"
             style={{ width: "14em", height: "10em" }}
           />
-        </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-            </div>
-          </div>
         </div>
       </footer>
     );
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,9 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link, graphql } from "gatsby";
+import { graphql } from "gatsby";
 
 import Layout from "../components/Layout";
-import Features from "../components/Features";
 
 export const IndexPageTemplate = ({
   image,
@@ -83,14 +82,6 @@ export const IndexPageTemplate = ({
                       {heading}
                     </h3>
                     <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
                   </div>
                 </div>
               </div>

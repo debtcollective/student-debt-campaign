aa45a990d8e41a475c8c41f4ef2049f2336a9925
Alexis Duran <alexis.duranm@gmail.com>
2019-08-23 20:08:55 +0200

    feat(FAQ): add FAQ section with some of the values it has

---
+++ src/sections/FAQ/index.js
@@ -0,0 +1,183 @@
+import React from "react";
+
+const FAQ = () => (
+  <section className="faq">
+    <div className="container-fluid distribute-rows">
+      <div className="row">
+        <div className="col">
+          <div className="text-center">
+            <h1 className="display-title">FAQ</h1>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className="collapsable-list">
+            <details className="collapsable-list__item">
+              <summary className="summary">Why full debt cancellation?</summary>
+              <p className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum hendrerit, erat id feugiat lobortis, enim nulla
+                pretium ex, eu auctor lacus eros in tortor. Sed justo metus,
+                sodales et lobortis id, sodales at nulla. In sed iaculis dui.
+                Mauris rhoncus convallis augue in volutpat. Aliquam sodales
+                tellus nec neque pretium, at auctor felis condimentum. Maecenas
+                placerat vestibulum tortor, nec consectetur nulla laoreet id.
+                Pellentesque vel velit pulvinar, blandit ex nec, tempus diam.
+                Phasellus dui massa, tincidunt et dictum vel, consequat sed
+                nisi. Mauris dapibus nunc sed neque molestie imperdiet. In
+                aliquam rhoncus metus eget ornare.
+              </p>
+            </details>
+            <details className="collapsable-list__item">
+              <summary className="summary">
+                Doesn’t full cancellation mostly beneﬁt higher income people?
+              </summary>
+              <p className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum hendrerit, erat id feugiat lobortis, enim nulla
+                pretium ex, eu auctor lacus eros in tortor. Sed justo metus,
+                sodales et lobortis id, sodales at nulla. In sed iaculis dui.
+                Mauris rhoncus convallis augue in volutpat. Aliquam sodales
+                tellus nec neque pretium, at auctor felis condimentum. Maecenas
+                placerat vestibulum tortor, nec consectetur nulla laoreet id.
+                Pellentesque vel velit pulvinar, blandit ex nec, tempus diam.
+                Phasellus dui massa, tincidunt et dictum vel, consequat sed
+                nisi. Mauris dapibus nunc sed neque molestie imperdiet. In
+                aliquam rhoncus metus eget ornare.
+              </p>
+            </details>
+            <details className="collapsable-list__item">
+              <summary className="summary">
+                What impact will cancelling student debt have on the economy?
+              </summary>
+              <p className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum hendrerit, erat id feugiat lobortis, enim nulla
+                pretium ex, eu auctor lacus eros in tortor. Sed justo metus,
+                sodales et lobortis id, sodales at nulla. In sed iaculis dui.
+                Mauris rhoncus convallis augue in volutpat. Aliquam sodales
+                tellus nec neque pretium, at auctor felis condimentum. Maecenas
+                placerat vestibulum tortor, nec consectetur nulla laoreet id.
+                Pellentesque vel velit pulvinar, blandit ex nec, tempus diam.
+                Phasellus dui massa, tincidunt et dictum vel, consequat sed
+                nisi. Mauris dapibus nunc sed neque molestie imperdiet. In
+                aliquam rhoncus metus eget ornare.
+              </p>
+            </details>
+            <details className="collapsable-list__item">
+              <summary className="summary">
+                OK, but how do we pay for it?
+              </summary>
+              <p className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum hendrerit, erat id feugiat lobortis, enim nulla
+                pretium ex, eu auctor lacus eros in tortor. Sed justo metus,
+                sodales et lobortis id, sodales at nulla. In sed iaculis dui.
+                Mauris rhoncus convallis augue in volutpat. Aliquam sodales
+                tellus nec neque pretium, at auctor felis condimentum. Maecenas
+                placerat vestibulum tortor, nec consectetur nulla laoreet id.
+                Pellentesque vel velit pulvinar, blandit ex nec, tempus diam.
+                Phasellus dui massa, tincidunt et dictum vel, consequat sed
+                nisi. Mauris dapibus nunc sed neque molestie imperdiet. In
+                aliquam rhoncus metus eget ornare.
+              </p>
+            </details>
+            <details className="collapsable-list__item">
+              <summary className="summary">
+                Does Congress Have to Approve Debt Relief?
+              </summary>
+              <p className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum hendrerit, erat id feugiat lobortis, enim nulla
+                pretium ex, eu auctor lacus eros in tortor. Sed justo metus,
+                sodales et lobortis id, sodales at nulla. In sed iaculis dui.
+                Mauris rhoncus convallis augue in volutpat. Aliquam sodales
+                tellus nec neque pretium, at auctor felis condimentum. Maecenas
+                placerat vestibulum tortor, nec consectetur nulla laoreet id.
+                Pellentesque vel velit pulvinar, blandit ex nec, tempus diam.
+                Phasellus dui massa, tincidunt et dictum vel, consequat sed
+                nisi. Mauris dapibus nunc sed neque molestie imperdiet. In
+                aliquam rhoncus metus eget ornare.
+              </p>
+            </details>
+            <details className="collapsable-list__item">
+              <summary className="summary">
+                What’s the problem with targeted relief?
+              </summary>
+              <p className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum hendrerit, erat id feugiat lobortis, enim nulla
+                pretium ex, eu auctor lacus eros in tortor. Sed justo metus,
+                sodales et lobortis id, sodales at nulla. In sed iaculis dui.
+                Mauris rhoncus convallis augue in volutpat. Aliquam sodales
+                tellus nec neque pretium, at auctor felis condimentum. Maecenas
+                placerat vestibulum tortor, nec consectetur nulla laoreet id.
+                Pellentesque vel velit pulvinar, blandit ex nec, tempus diam.
+                Phasellus dui massa, tincidunt et dictum vel, consequat sed
+                nisi. Mauris dapibus nunc sed neque molestie imperdiet. In
+                aliquam rhoncus metus eget ornare.
+              </p>
+            </details>
+            <details className="collapsable-list__item">
+              <summary className="summary">
+                Isn’t debt cancellation unfair to people who have already paid
+                back all of their student loans?
+              </summary>
+              <p className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum hendrerit, erat id feugiat lobortis, enim nulla
+                pretium ex, eu auctor lacus eros in tortor. Sed justo metus,
+                sodales et lobortis id, sodales at nulla. In sed iaculis dui.
+                Mauris rhoncus convallis augue in volutpat. Aliquam sodales
+                tellus nec neque pretium, at auctor felis condimentum. Maecenas
+                placerat vestibulum tortor, nec consectetur nulla laoreet id.
+                Pellentesque vel velit pulvinar, blandit ex nec, tempus diam.
+                Phasellus dui massa, tincidunt et dictum vel, consequat sed
+                nisi. Mauris dapibus nunc sed neque molestie imperdiet. In
+                aliquam rhoncus metus eget ornare.
+              </p>
+            </details>
+            <details className="collapsable-list__item">
+              <summary className="summary">What is education for?</summary>
+              <p className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum hendrerit, erat id feugiat lobortis, enim nulla
+                pretium ex, eu auctor lacus eros in tortor. Sed justo metus,
+                sodales et lobortis id, sodales at nulla. In sed iaculis dui.
+                Mauris rhoncus convallis augue in volutpat. Aliquam sodales
+                tellus nec neque pretium, at auctor felis condimentum. Maecenas
+                placerat vestibulum tortor, nec consectetur nulla laoreet id.
+                Pellentesque vel velit pulvinar, blandit ex nec, tempus diam.
+                Phasellus dui massa, tincidunt et dictum vel, consequat sed
+                nisi. Mauris dapibus nunc sed neque molestie imperdiet. In
+                aliquam rhoncus metus eget ornare.
+              </p>
+            </details>
+            <details className="collapsable-list__item">
+              <summary className="summary">
+                What kind of beneﬁts can debt cancellation have for society and
+                the economy?
+              </summary>
+              <p className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum hendrerit, erat id feugiat lobortis, enim nulla
+                pretium ex, eu auctor lacus eros in tortor. Sed justo metus,
+                sodales et lobortis id, sodales at nulla. In sed iaculis dui.
+                Mauris rhoncus convallis augue in volutpat. Aliquam sodales
+                tellus nec neque pretium, at auctor felis condimentum. Maecenas
+                placerat vestibulum tortor, nec consectetur nulla laoreet id.
+                Pellentesque vel velit pulvinar, blandit ex nec, tempus diam.
+                Phasellus dui massa, tincidunt et dictum vel, consequat sed
+                nisi. Mauris dapibus nunc sed neque molestie imperdiet. In
+                aliquam rhoncus metus eget ornare.
+              </p>
+            </details>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default FAQ;

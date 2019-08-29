b84ec31d1756bca9fb98fcf22cb0315376f29b6e
Alexis Duran <alexis.duranm@gmail.com>
2019-08-29 07:55:22 +0200

    feat(cta): add section with responsive implications

---
+++ src/sections/CTA/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CTA = () => (
+  <section className="cta">
+    <div className="container-fluid distribute-rows justify-content-center">
+      <div className="row">
+        <div className="col">
+          <div className="text-center">
+            <h2 className="display-title mb-5">
+              Help Build Power. Join Today!
+            </h2>
+            <button className="btn btn-primary mt-lg-5">Sign up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default CTA;

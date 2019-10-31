c5f8994a84031cc06c092080b9f75597e7fac7c9
Alexis Duran <alexis.duranm@gmail.com>
2019-10-31 21:36:44 +0100

    refactor(campaignAction): add new component to be used to render specific actions types

---
+++ src/components/CampaignAction.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const CampaignAction = ({ config }) => {
+  const { text, delay, ...attributes } = config;
+
+  return (
+    <div className="cta-container content">
+      <a data-testid="action-cta" {...attributes}>
+        {text}
+      </a>
+    </div>
+  );
+};
+
+CampaignAction.propTypes = {
+  text: PropTypes.string,
+  config: PropTypes.any
+};

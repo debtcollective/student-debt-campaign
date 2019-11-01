b2a3c5d7af0b725e9152c02d221c157c9864af3e
Alexis Duran <alexis.duranm@gmail.com>
2019-11-01 15:44:30 +0100

    feat(campaignActions): add api module to handle queries

---
+++ src/sections/CampaignActions/api.js
@@ -0,0 +1,27 @@
+import gql from "graphql-tag";
+
+export const GET_CAMPAIGN_ACTIONS = gql`
+  query getUserActions($userId: ID!, $campaignId: ID!) {
+    userCampaignsActions(userId: $userId, campaignId: $campaignId) {
+      campaignId
+      config
+      description
+      id
+      title
+      type
+    }
+  }
+`;
+
+export const GET_USER_ACTIONS = gql`
+  query getUserActions($campaignId: ID!, $actionId: ID!) {
+    userActions(
+      userId: 1
+      filterBy: { campaignId: $campaignId, actionId: $actionId }
+    ) {
+      actionId
+      completed
+      id
+    }
+  }
+`;

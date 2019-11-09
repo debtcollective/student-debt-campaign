f2a5da2cd5ca631df6e2c3766e44a74422d94f8d
Alexis Duran <alexis.duranm@gmail.com>
2019-11-09 13:12:25 +0100

    refactor(apollo): add support to send cookies correctly

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,15 @@
-import ApolloClient from "apollo-boost";
+import ApolloClient from "apollo-client";
 import fetch from "isomorphic-fetch";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const link = createHttpLink({
+  uri: `${process.env.GATSBY_CAMPAING_ACTIONS_API}`,
+  credentials: "include"
+});
 
 export const client = new ApolloClient({
-  uri: `${process.env.GATSBY_CAMPAIGN_API_URL}`,
+  cache: new InMemoryCache(),
+  link,
   fetch
 });

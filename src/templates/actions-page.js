e2ceb18933d573715e894596e29f45109e74a25e
Alexis Duran <alexis.duranm@gmail.com>
2019-11-09 13:13:29 +0100

    chore(actions): add minimal code to check that request on actions take place as expected

--- src/templates/actions-page.js
+++ src/templates/actions-page.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
 import Layout from "../components/Layout";
 import Campaign from "../sections/CampaignActions";
@@ -10,11 +12,36 @@ const tempFakeUser = {
   email: "jane.doe@mail.com"
 };
 
-export const ActionsPageTemplate = () => (
-  <>
-    <Campaign user={tempFakeUser} campaignId="8" />
-  </>
-);
+const GET_USER = gql`
+  query getUser {
+    currentUser {
+      id
+    }
+  }
+`;
+
+export const ActionsPageTemplate = () => {
+  const {
+    loading: userQueryLoading,
+    error: userQueryError,
+    data: userQueryResponse
+  } = useQuery(GET_USER);
+
+  console.log({
+    userQueryLoading,
+    userQueryError,
+    userQueryResponse
+  });
+
+  return (
+    <>
+      {userQueryLoading
+        ? "Loading"
+        : (userQueryError && userQueryError.message) ||
+          JSON.stringify(userQueryResponse.currentUser)}
+    </>
+  );
+};
 
 const ActionsPage = () => {
   return (

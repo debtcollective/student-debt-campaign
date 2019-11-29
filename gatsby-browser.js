e93ef6a3e7ea2589881bc5377808d587086fe7ac
Alexis Duran <alexis.duranm@gmail.com>
2019-11-29 14:07:56 +0100

    feat(share): add base to integrate 'addthis.com' service

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
 export { wrapRootElement } from './src/apollo/wrap-root-element'
+
+export const onInitialClientRender = () => {
+  const addThisScript = document.createElement('script')
+  addThisScript.src = `//s7.addthis.com/js/300/addthis_widget.js#pubid=${process.env.GATSBY_ADDTHIS_PUB_ID}`
+  document.body.appendChild(addThisScript)
+}

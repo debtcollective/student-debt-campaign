95401a5abc6e3b15f22f4fe47ae942d98b9ddc63
Alexis Duran <alexis.duranm@gmail.com>
2019-12-17 10:10:03 +0100

    feat(config): allow to map collections folder to be "query-able"

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,13 @@ module.exports = {
         name: 'pages'
       }
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/collections`,
+        name: 'collections'
+      }
+    },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
